d78d8d2351b44fbc625d09f8e2ee06260becbba0
Steve Clay <steve@mrclay.org>
2014-09-08 14:45:34 -0400

    Adds view to display content from a page

---
+++ views/default/anypage/content.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Display the content from a page
+ *
+ * @uses $vars['path'] The CMS page path
+ */
+
+$path = $vars['path'];
+
+$page = AnyPage::getAnyPageEntityFromPath($path);
+if (!$page) {
+	echo "<!-- no content for path $path -->";
+	return;
+}
+
+echo $obj->description;
